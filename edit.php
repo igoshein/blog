a5d666e9b395d6784cb252430f779a22f2101d6b
igoshein@gmail.com <igoshein@gmail.com>
2017-08-04 11:16:23 +0300

    Add files

---
+++ edit.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Get All files HTML
+ * @return array files.html
+ */
+function getDirContents($dir, &$files_path = array())
+{
+
+    foreach (scandir($dir) as $key => $value) {
+        $path = realpath($dir . DIRECTORY_SEPARATOR . $value);
+        if (!is_dir($path)) {
+            if (strripos($path, '.html')) {
+                $files_path[] = $path;
+            }
+        } else if ($value != "." && $value != "..") {
+            getDirContents($path, $files_path);
+            if (strripos($path, '.html')) {
+                $files_path[] = $path;
+            }
+        }
+    }
+
+    return $files_path;
+}
+
+$count = 0;
+foreach (getDirContents(__DIR__) as $key => $path) {
+
+    $search_string = '<html><!--Test-->';
+
+    $replace_string = '<html>';
+
+    $content_file = file_get_contents($path);
+    $content_new = str_replace($search_string, $replace_string, $content_file);
+    file_put_contents($path, $content_new);
+    $count++;
+}
+
+echo 'Done - edit ' . $count . ' files' . PHP_EOL;
